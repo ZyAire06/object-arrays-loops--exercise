af0fc32c910fedbb79eb75b380ccfbe5d638b0f8
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-11-01 15:34:49 -0400

    loops exericse add while_loop.js, for_loop.js, and loops-ex.html

---
+++ loops/LoopScripts/for_loops.js
@@ -0,0 +1,8 @@
+// let love = 'I love Loops';
+
+for (let i=0; i < 7; i++) {
+    let love = 'I love Loops';
+     console.log(love);
+     console.log(i);
+    
+}
---
+++ loops/LoopScripts/while_loops.js
@@ -0,0 +1,9 @@
+let i = 0;
+let love = 'I love Loops';
+
+while (i<7) {
+    i++;
+    console.log(i);
+    
+    console.log(love);
+}
